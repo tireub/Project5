88552a8f25d6f3a1f2c3cb662be4dddf09ee6ee6
tireub <tireub@msn.com>
2017-11-06 11:13:16 +0100

    Finished database import functions. Possible improvement on tests to avoid double entries.

--- apiimport.py
+++ apiimport.py
@@ -17,11 +17,11 @@ from Functions import*
 
 
 
-#resp = requests.get('https://world.openfoodfacts.org/country/france/1000.json')
+resp = requests.get('https://world.openfoodfacts.org/country/france/300.json')
 #print (resp.json())
 
-#c = resp.json()['products'][5] #['categories']
-#print(c)
+plop = resp.json()['products'][18] #['categories']
+#print(plop)
 #print(c.split(','))
 
 #aliment_test = Product()
@@ -30,13 +30,54 @@ from Functions import*
 #print(aliment_test.link)
 #print(aliment_test.categories)
 
+#print(aliment_test.categories[1])
+#print(index)
 #cat = Category()
 #tst = requests.get('https://fr.openfoodfacts.org/categories.json')
 #cat.load(tst.json()['tags'][18])
 
 
 test = Categories()
-test.overview()
+db = MySQLdb.connect(host="localhost",user="root",passwd="Davemurray33",db="foodfacts")
+#test = Product()
+#test.jsonread(plop)
+#test.parenthood_fill(db)
+
+#print(len(''))
+#test.aliment_fill(db)
+#c = db.cursor()
+#query = ("SELECT id FROM Categories WHERE name = 'Sodas light'")
+#c.execute(query)
+#print(c.fetchall())
+#tuple = []
+#for id in c:
+#    print(id)
+#    tuple.append(id)
+#print(tuple[0])
+
+#test = Product()
+#test.categories = ('Aliments et boissons à base de végétaux', "Aliments d'origine végétale", 'Epicerie',
+#                   'Produits déshydratés', 'Bouillons', 'Produits lyophilisés à reconstituer', 'Bouillons déshydratés',
+#                   'Bouillons de légumes', 'Bouillons cubes')
+#test.categories = list(map(str.encode('ascii')[:39], unicodedata.normalize('NFKD', test.categories)))
+#print(test.categories)
+
+
+#test.name = 'alimenttest'
+#test.link = 'openclassrooms.com'
+#test.grade = 2
+#test.parenthood_fill(db)
+#test.aliment_fill(db)
+
+#print(test.categories)
+#test.parenthood_fill(db)
+#print(test.finest_cat)
+
+
+
+
+
+#test.overview(db)
 #print(cat.name, cat.off_id, cat.elem_count)
 
-
+fillelements(db)
--- functions.py
+++ functions.py
@@ -11,45 +11,110 @@ class Product:
         self.name = ''
         self.link = ''
         self.categories = ''
+        self.finest_cat = ''
+        self.finest_cat_id = 1
         self.ingredients = ''
-        self.grade = 0
+        self.grade = 6
         self.brand = ''
         self.stores = ''
 
     #def import from json
     def jsonread(self, json_file):
         if 'product_name_fr' in json_file:
-            self.name = json_file['product_name_fr']
+            self.name = unicodedata.normalize('NFKD', json_file['product_name_fr']).encode('ascii', 'ignore')[:39]
         if 'code' in json_file:
             self.link = 'https://fr.openfoodfacts.org/produit/'+json_file['code']
-        if 'categories' in json_file:
-            self.categories = json_file['categories']
         if 'ingredients' in json_file:
-            self.ingredients = json_file['ingredients']
+            self.ingredients = unicodedata.normalize('NFKD', str(json_file['ingredients'])).encode('ascii', 'ignore')
         if 'nutrition_grade_fr' in json_file:
-            self.grade = json_file['nutrition_grade_fr']
+            if json_file['nutrition_grade_fr'] == 'a':
+                self.grade = 1
+            elif json_file['nutrition_grade_fr'] == 'b':
+                self.grade = 2
+            elif json_file['nutrition_grade_fr'] == 'c':
+                self.grade = 3
+            elif json_file['nutrition_grade_fr'] == 'd':
+                self.grade = 4
+            elif json_file['nutrition_grade_fr'] == 'e':
+                self.grade = 5
         if 'brands' in json_file:
-            self.brand = json_file['brands']
+            self.brand = unicodedata.normalize('NFKD', json_file['brands']).encode('ascii', 'ignore')[:39]
         if 'sores' in json_file:
-            self.stores = json_file['stores']
+            self.stores = unicodedata.normalize('NFKD', json_file['stores']).encode('ascii', 'ignore')[:39]
         if 'categories' in json_file:
             self.categories = json_file['categories'].split(',')
+            self.categories = list(map(str.lstrip, self.categories))
+            tmp = []
+            for cat in self.categories:
+                tmp.append(unicodedata.normalize('NFKD', cat).encode('ascii', 'ignore')[:39])
+            self.categories = tmp
 
-    #def insert into database
-    #def database_insert(self, basename):
+    #Insertion of the aliment data in db
+    def aliment_fill(self, db):
+        c = db.cursor()
+        #Test if the element is already in the database
+        if c.execute("SELECT id FROM Elements WHERE"
+                     "(name, link) = (%s, %s)", (self.name, self.link)) == 0:
+            #If not, fill the database
+            c.execute("INSERT INTO Elements VALUES (NULL, %s, %s, %s, %s, %s, %s, %s)",
+                      (self.name, self.link, self.finest_cat_id, self.ingredients, self.grade,
+                       self.brand, self.stores))
+        db.commit()
+
+
+    #def insert categories parenthood into database
+    def parenthood_fill(self, db):
+        c = db.cursor()
+        #print(len(self.categories))
+        #print(self.categories)
+        if len(self.categories) > 1:
+
+            for a in range(len(self.categories) - 1):
+                temp = []
+                #print(self.categories[a])
+                #print(self.categories[a+1])
+                c.execute("SELECT id FROM Categories WHERE name IN (%s, %s)"
+                          "OR off_id IN (%s, %s)",
+                          (self.categories[a], self.categories[a+1], self.categories[a], self.categories[a+1]))
+
+                for id in c:
+                    temp.append(id)
+
+                if len(temp) == 2:
+                    if c.execute("SELECT category_id FROM Parenthood WHERE"
+                                 "(category_id, parent_category_id) = (%s, %s)", (temp[1], temp[0])) == 0:
+                        c.execute("INSERT IGNORE INTO Parenthood VALUES (%s, %s)", (temp[1], temp[0]))
+            self.finest_cat = self.categories[a+1]
+        elif len(self.categories) == 1:
+            self.finest_cat = self.categories
+
+            c.execute("SELECT id FROM Categories WHERE name IN (%s, %s)",
+                  (self.finest_cat, self.finest_cat))
+            for id in c:
+                self.finest_cat_id = id
+
+        db.commit()
+
+
+
+    #def database_insert(self):
 
 class Categories:
 
-    def overview(self):
+    def overview(self, db):
+        #Extracts all the categories from OFF and fills in database
+        #Only to be used while generating database
         tst = requests.get('https://fr.openfoodfacts.org/categories.json')
         max = tst.json()['count']
-        db = MySQLdb.connect(host="localhost",user="root",passwd="Davemurray33",db="foodfacts")
+
         for a in range(max):
             tmp = Category()
             tmp.load(tst.json()['tags'][a])
             print(tmp.name)
             print(a)
             tmp.database_insert(db)
+        c = db.cursor()
+        c.execute("DELETE FROM Categories WHERE name = ''")
         db.commit()
 
 class Category:
@@ -72,5 +137,16 @@ class Category:
         c = database_connection.cursor()
         c.execute("INSERT INTO Categories VALUES (NULL, %s, %s, %s)",
                   (self.name, self.off_id, self.elem_count))
+
+def fillelements(database):
+    for a in range(1, 7184):
+        link = ('https://world.openfoodfacts.org/country/france/%d.json' %a)
+        print(a)
+        resp = requests.get(link)
+        for element in resp.json()['products']:
+            test = Product()
+            test.jsonread(element)
+            test.parenthood_fill(database)
+            test.aliment_fill(database)
 
 
